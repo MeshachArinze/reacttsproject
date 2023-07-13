f93317e12975508b1655d144d8f7cb58fee57d33
MeshachArinze <meshacharinze@gmail.com>
2023-07-13 10:36:21 +0100

    commit to repo

--- agency/src/components/Work.tsx
+++ agency/src/components/Work.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import Nav from './Nav';
 
-const Work: React.FC = () => {
+const Work = () => {
   return (
     <div>
       <header>
